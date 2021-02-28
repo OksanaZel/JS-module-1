7c475a9823c62942eeb68e8f4cf27ed94a56d8b2
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-02-28 13:51:55 +0200

    Добавляет задачи

--- js/01module-hw-1.js
+++ js/01module-hw-1.js
@@ -180,29 +180,42 @@
 //Индия - 80 кредитов
 //Ямайка - 120 кредитов
 
-const countryName = "КитаЙ";
+// const countryName = null;
 
-const CANCELED_BY_USER = 'Отменено пользователем!';
-const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
-const CHINA = 'Китай';
-const AUSTRALIA = 'Австралия';
-const INDIA = 'Индия';
-const JAMAICA = 'Ямайка';
-let message;
-let price = 0;
-let country;
-if (countryName === null) {
-  message = CANCELED_BY_USER;
-} else {
-  country = countryName[0] + countryName.slice(); // Write code on this line
-  switch (country) {
-  // Write code under this line
-    
-  }
-}
-if () { // Write code on this line
-  message = `Доставка в ${country} будет стоить ${price} кредитов`;
-}
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
+// const CHINA = 'Китай';
+// const AUSTRALIA = 'Австралия';
+// const INDIA = 'Индия';
+// const JAMAICA = 'Ямайка';
+// let message;
+// let price = 0;
+// let country;
+// if (countryName === null) {
+//   message = 'Отменено пользователем!';
+// } else {
+//   country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
+//   switch (country) {
+//     case CHINA:
+//       price = 100;
+//       break;
+//     case AUSTRALIA:
+//       price = 170;
+//       break;
+//     case INDIA:
+//       price = 80;
+//       break;
+//     case JAMAICA:
+//       price = 120;
+//       break;
+//   }
+
+//   if (price > 0) {
+//     message = `Доставка в ${country} будет стоить ${price} кредитов`;
+//   } else {
+//     message = NO_DELIVERY;
+//   }
+// }
 
 // console.log(message);
 
